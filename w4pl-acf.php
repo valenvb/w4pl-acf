9037d57151f868fc7f30e734892ee3820ea6cceb
Valen Varangu Booth <valen@valenvb.ca>
2020-01-04 14:34:21 -0500

    Boilerplate

---
+++ w4pl-acf.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: W4 Post List - Advanced Custom Fields
+ * Description: Provides access to ACF data in W4 Post List
+ * Author: Valen Varangu-Booth
+ * Version: 0.0.1-alpha
+ */
+
+ 

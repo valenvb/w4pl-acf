764f15fdbe69788960c793fb54ae98957028cdaa
Valen Varangu Booth <valen@valenvb.ca>
2020-01-04 14:38:00 -0500

    add license

--- w4pl-acf.php
+++ w4pl-acf.php
@@ -4,6 +4,7 @@
  * Description: Provides access to ACF data in W4 Post List
  * Author: Valen Varangu-Booth
  * Version: 0.0.1-alpha
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
- 

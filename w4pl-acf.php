1fb09b09a95702d133a13badce5a6f82162fee44
Valen Varangu Booth <valen@valenvb.ca>
2020-01-04 18:23:09 -0500

    register w4pl shortcode

--- w4pl-acf.php
+++ w4pl-acf.php
@@ -8,3 +8,19 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
+
+if( !function_exists("w4pl_acf_load_shortcode") ){
+    function w4pl_acf_load_shortcode($codes){
+        $acf_code = array(
+            'acf' => array(
+                'group' => 'Post',
+                'callback' => 'w4pl_acf_data_callback',
+                'desc' => '<strong>'. __('Output', 'w4pl') .'</strong>: ACF field data'
+            )
+        );
+
+        return array_merge($codes, $acf_code);
+    }
+    //register shortcode with W4PL
+    add_filter('w4pl/get_shortcodes', 'w4pl_acf_load_shortcode');
+}

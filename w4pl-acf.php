54093047591859204ed1c0612dbd2c87ffd2f67c
Valen Varangu Booth <valen@valenvb.ca>
2020-01-04 19:20:14 -0500

    Get and return ACF data

--- w4pl-acf.php
+++ w4pl-acf.php
@@ -24,3 +24,31 @@ if( !function_exists("w4pl_acf_load_shortcode") ){
     //register shortcode with W4PL
     add_filter('w4pl/get_shortcodes', 'w4pl_acf_load_shortcode');
 }
+
+if( !function_exists('w4pl_acf_data_callback') ){
+    function w4pl_acf_data_callback($attr, $cont){
+        if( !is_string($attr[0]) && !isset($attr['field']) ){
+            error_log("w4pl_acf: no ACF key given");
+            return "";
+        }
+        if( is_string($attr[0]) ){
+            $acf_key = $attr[0];
+        } else {
+            $acf_key = $attr['field'];
+        }
+
+        $acf_key = preg_split("/\./", $acf_key);
+        
+        $acf_data = get_fields();
+
+        $result = array_reduce($acf_key, 'w4pl_acf_util_keyReduce', $acf_data);
+
+        return $result;
+    }
+}
+
+if( !function_exists('w4pl_acf_util_keyReduce') ){
+    function w4pl_acf_util_keyReduce(array $arr, $idx){
+        return array_key_exists($idx, $arr) ? $arr[$idx] : null;        
+    }
+}

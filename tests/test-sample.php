ba46cfceb1be200283489098dfe0d08641a2e613
Valen Varangu Booth <valen@valenvb.ca>
2020-01-05 18:39:19 -0500

    default phpunit tests

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package W4pl_Acf
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}

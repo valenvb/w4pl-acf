f670e0c0896667ec75da69bd0163bef321311c65
Valen Varangu Booth <valen@valenvb.ca>
2020-01-04 20:46:31 -0500

    version 1

--- w4pl-acf.php
+++ w4pl-acf.php
@@ -3,7 +3,7 @@
  * Plugin Name: W4 Post List - Advanced Custom Fields
  * Description: Provides access to ACF data in W4 Post List
  * Author: Valen Varangu-Booth
- * Version: 0.0.1-alpha
+ * Version: 1.0.0
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
